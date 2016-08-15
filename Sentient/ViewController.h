31517166cd42b1029b520705c327c3340a17d535
Konstantin Stolyarenko <stolyarenko.kons@mail.ru>
2016-08-16 02:41:18 +0300

    Initial Commit

---
+++ Sentient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sentient
+//
+//  Created by Kostya on 16.08.16.
+//  Copyright © 2016 Stolyarenko K.S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
